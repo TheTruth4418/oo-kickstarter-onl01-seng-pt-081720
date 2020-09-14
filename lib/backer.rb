875c328cd375521fcc3c01af988f3e906363f1fd
William West <wwest466@gmail.com>
2020-09-14 14:46:49 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,11 +1,19 @@
 class Backer
   attr_accessor :name
   attr_reader :backed_projects
+  @@backers = []
   def initialize(name)
     @name = name
     @backed_projects = []
   end
   def back_project(project)
     @backed_projects << project
+  end
+  def add_backer(name)
+    backer = Backer.new(name)
+    self.backers << backer
+  end
+  def self.backers
+    @@backers
   end
 end
