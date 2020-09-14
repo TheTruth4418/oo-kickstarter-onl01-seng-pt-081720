4e23800131f48c64fc436acb4134e5eed06a3741
William West <wwest466@gmail.com>
2020-09-14 14:37:18 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,9 +1,7 @@
 class Project
-  attr_accessor :title
+  attr_accessor :title, :backers
   def initialize(title)
     @title = title
-  end
-  def backers
-    @backed_projects
+    @backers = []
   end
 end
